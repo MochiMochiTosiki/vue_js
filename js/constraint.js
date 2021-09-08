cde2bca1ecb88de3230625c5070ba3676f4a55b6
toshiki <mochi.mochi.0397@gmail.com>
2021-09-08 11:23:40 +0900

    2-3-2 リアクティブシステムの制約(2-23, 2-24)

---
+++ js/constraint.js
@@ -0,0 +1,19 @@
+new Vue({
+    el: '#app',
+    data: {
+        author: {
+            name: '山田',
+        }
+    },
+    created: function() {
+        let that = this;
+        this.timer = setTimeout(function() {
+            // that.author.name = 'Y.YAMADA'
+            Vue.set(that.author, 'company', 'WINGSプロジェクト');
+        }, 3000);
+    },
+
+    beforeDestroy: function() {
+        clearInterval(this.timer);
+    }
+});
