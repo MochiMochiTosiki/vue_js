a17a6900be02e71434dba6b13bd1505d365aa5fc
toshiki <mochi.mochi.0397@gmail.com>
2021-09-09 11:38:15 +0900

    3-4-1 属性に値をバインドする(3-66 ~ 3-71)

---
+++ chapter3/js/bind_obj.js
@@ -0,0 +1,13 @@
+new Vue({
+    el: '#app',
+    data: {
+      attrs: {
+        size: 20,
+        maxlength: 14,
+        require: true
+      },
+      text: 'みなさん、こんにちは!',
+      attr: 'width',
+      size: 100
+    }
+})
