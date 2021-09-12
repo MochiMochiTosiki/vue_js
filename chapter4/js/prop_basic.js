782556041b71550565930c9040bb33ba7ba79bf1
toshiki <mochi.mochi.0397@gmail.com>
2021-09-13 00:19:42 +0900

    4-2-3 プロパティの方を制限する

--- chapter4/js/prop_basic.js
+++ chapter4/js/prop_basic.js
@@ -1,5 +1,13 @@
 Vue.component('my-hello', {
-  props: ['yourName'],
+  props: {
+    yourName: {
+      type: String,
+      required: true,
+      validator: function(value) {
+        return value.length <= 5;
+      }
+    }
+  },
   template: '<div>こんにちは、{{ yourName }}さん！</div>'
 });
 
