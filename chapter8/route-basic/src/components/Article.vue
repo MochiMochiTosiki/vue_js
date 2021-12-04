58f89819946654a14552d207366354d82bf81048
toshiki <mochi.mochi.0397@gmail.com>
2021-12-04 23:06:28 +0900

    8-4-3 ルート遷移時に処理を差し挟む

--- chapter8/route-basic/src/components/Article.vue
+++ chapter8/route-basic/src/components/Article.vue
@@ -9,8 +9,28 @@
 </template>
 
 <script>
+let timeGuard = function(to, from, next) {
+    let data = {
+        13: new Date(2019, 10, 30),
+        108: new Date(2018, 10, 30)
+    }
+
+    let limit = data[to.params.aid] ? data[to.params.aid]: new Date(2999, 12, 31)
+
+    let current = new Date()
+
+    if (limit && limit.getTime() > current.getTime()) {
+        next()
+    } else {
+        window.alert('記事の公開期限が過ぎています')
+        next(false)
+    }
+}
+
 export default {
     name: 'Article',
+    beforeRouteEnter: timeGuard,
+    beforeRouteUpdate: timeGuard
     props: {
         aid: String
     }
